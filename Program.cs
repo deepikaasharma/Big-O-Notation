892f6dddbea593dc5c7f06ca77c34d78e9849b7b
Deepika Sharma <Deepikaasharma@outlook.com>
2020-03-24 14:30:26 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_O_Notation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void printFirstItem(int[] items)  // this method runs in O(1) time, this method requires just one step even if input is huge
+        {
+            Console.WriteLine(items[0]);
+        }
+
+        public void printAllItems(int[] items) // this method runs in O(n), Linear time where n is the number of items in the array
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void printAllPossibleOrderedPairs(int[] items) // this method runs in O(n^2) time, because we use two nested loops
+        {
+            foreach (var firstitem in items)
+            {
+                foreach (var seconditem in items)
+                {
+                    Console.WriteLine($"{firstitem}, {seconditem}");
+                }
+            }
+        }
+    }
+}
